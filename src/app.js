e3b52f6147be2b0ee106865499001ba73c3b6cbd
sbustos497 <sbustos629@unab.edu.co>
2023-12-06 09:17:45 -0500

    cambios1

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import contactosRoutes from "./routes/contactos.routes.js";
 
@@ -7,12 +8,13 @@ const app = express();
 
 //middlewares
 app.use(express.json());
-
+app.use(cors());
 app.use((req , res , next) => {
     res.status(404).json({
         message: "Not Found"
     });
 });
+
 
 
 app.use(contactosRoutes);
