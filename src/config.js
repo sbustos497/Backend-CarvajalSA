b380ec23e1c6450fd848a0aa18b8f5091af660d2
sbustos497 <sbustos629@unab.edu.co>
2023-12-06 01:27:53 -0500

    script changes1

--- src/config.js
+++ src/config.js
@@ -6,3 +6,5 @@ export const DB_NAME = process.env.DB_NAME || 'practicas';
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASS = process.env.DB_PASS || '12345678';
 export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_PORT = process.env.DB_PORT || '3306';
+
