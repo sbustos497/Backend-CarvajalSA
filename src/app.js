d9fab78055db40585d128647cb418f0f683684aa
sbustos497 <sbustos629@unab.edu.co>
2023-12-06 09:20:56 -0500

    cambios1

--- src/app.js
+++ src/app.js
@@ -9,11 +9,7 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(cors());
-app.use((req , res , next) => {
-    res.status(404).json({
-        message: "Not Found"
-    });
-});
+
 
 
 
