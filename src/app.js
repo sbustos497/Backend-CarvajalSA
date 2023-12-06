ba15f557aaf5283a68cb5aeafea8711c6dfb37b6
sbustos497 <sbustos629@unab.edu.co>
2023-12-06 09:03:44 -0500

    cambios1

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from 'cors';
 
 import contactosRoutes from "./routes/contactos.routes.js";
 
@@ -15,10 +14,6 @@ app.use((req , res , next) => {
     });
 });
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  }));
 
 app.use(contactosRoutes);
 
