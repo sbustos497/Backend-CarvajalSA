f86a25ab8ab75c6ef9278193233198a02490aed3
sbustos497 <sbustos629@unab.edu.co>
2023-12-06 02:05:46 -0500

    cambios

--- src/Database/db.js
+++ src/Database/db.js
@@ -1,7 +1,21 @@
 import Sequalize from 'sequelize';
 import {DB_HOST,DB_NAME,DB_PASS,DB_USER} from '../config.js';
 
-export const sequelize = new Sequalize (DB_NAME, DB_USER, DB_PASS, {
-    host: DB_HOST,
-    dialect: 'mysql'
+export const sequelize = new Sequalize(
+    DB_NAME,
+    DB_USER,
+    DB_PASS,
+    {
+        host: DB_HOST,
+        dialect: 'mysql'
+    }
+);
+
+sequelize.authenticate()
+.then(() => {
+    console.log('Conectado a la base de datos');
+})
+.catch((error) => {
+    console.log('Error de conexion: '+error);
 });
+
